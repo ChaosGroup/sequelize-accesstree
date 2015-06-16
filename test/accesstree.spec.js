948d42b569a0901a8fd456578203d7462b0d2620
Valeri Bogdanov <valeribogdanov@gmail.com>
2015-06-16 16:36:39 +0300

    test added for scope. Failing due to limitation in sequelize-hierarchy

--- test/accesstree.spec.js
+++ test/accesstree.spec.js
@@ -8,7 +8,7 @@ const db = require('./dbinit');
 const AccessTree = db.AccessTree;
 const Users = require('./Users');
 
-const AccessTreeGrants = db.sequelize.models.AccessTreeGrant;
+const AccessTreeGrant = db.sequelize.models.AccessTreeGrant;
 const treeSetup = require('./treeSetup');
 
 describe('Access Tree with example model', function () {
@@ -17,7 +17,7 @@ describe('Access Tree with example model', function () {
 	let nodes;
 
 	beforeEach(function* () {
-		nodes = yield treeSetup(AccessTree, AccessTreeGrants);
+		nodes = yield treeSetup(AccessTree, AccessTreeGrant);
 	});
 
 	it('can get all the nodes', function* () {
@@ -40,5 +40,39 @@ describe('Access Tree with example model', function () {
 		let betaSalesAmericas = yield AccessTree.scope('withgrants').findById(nodes.betaSalesAmericas.id);
 		let grants = betaSalesAmericas.AccessTreeGrants;
 		expect(grants).to.be.an('array').with.length(2);
+		expect(_.map(grants, 'UserId')).to.have.members([Users.eve, Users.peter]);
+	});
+
+	it('returns subtree and grants if both scopes are enabled', function* () {
+		//let tmp = AccessTree.scope('withgrants');
+		let beta = yield AccessTree.scope('withgrants').findOne({
+				where: {
+					id: nodes.beta.id
+				},
+				include: [
+					/*{
+						model: AccessTreeGrant,
+						as: 'AccessTreeGrants'
+					},*/ {
+						model: AccessTree.scope('withgrants'),
+						as: 'descendents',
+						hierarchy: true/*,
+						include: [
+							{
+								model: AccessTreeGrant,
+								as: 'AccessTreeGrants'
+							}
+						]*/
+					}
+				]
+			});
+
+		expect(beta.AccessTreeGrants).to.be.an('array').with.length(2);
+		expect(beta.children).to.be.an('array').with.length(1);
+
+		let sales = beta.children[0];
+		console.log(sales);
+		expect(sales.name).to.equal('Sales');
+		expect(sales.AccessTreeGrants).to.be.an('array').with.length(1);
 	});
 });
